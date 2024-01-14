fd5cb814ac17309cca2ecd00445265b8a53ec3c6
nandar-theint <nanda.tein.teinto@medical-frontier.jp>
2024-01-14 11:42:26 +0900

    add new project to en

--- script/main.js
+++ script/main.js
@@ -2,11 +2,4 @@
 
 $(function() {
   AOS.init();
-  console.log('OK');
 });
-
-function scrollToTargetElement(elementId) {
-  $('html, body').animate({
-    scrollTop: $(`#${elementId}`).offset().top
-  }, 500);
-}
